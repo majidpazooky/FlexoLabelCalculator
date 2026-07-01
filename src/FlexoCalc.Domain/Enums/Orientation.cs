0d446413d6fad80153b02a3172e70cd23d19459e
majidpazooky <43704313+majidpazooky@users.noreply.github.com>
2026-07-01 10:18:25 +0330

    Add Orientation enum

--- src/FlexoCalc.Domain/Enums/Orientation.cs
+++ src/FlexoCalc.Domain/Enums/Orientation.cs
@@ -1,1 +1,24 @@
+namespace FlexoCalc.Domain.Enums;
 
+/// <summary>
+/// جهت قرارگیری لیبل روی سیلندر چاپ
+/// </summary>
+public enum Orientation
+{
+    /// <summary>
+    /// حالت ایستاده
+    /// طول لیبل در امتداد حرکت کاغذ (محیط سیلندر) قرار می‌گیرد.
+    /// </summary>
+    Portrait = 0,
+
+    /// <summary>
+    /// حالت خوابیده
+    /// عرض لیبل در امتداد حرکت کاغذ (محیط سیلندر) قرار می‌گیرد.
+    /// </summary>
+    Landscape = 1,
+
+    /// <summary>
+    /// هر دو حالت بررسی شوند و بهترین نتیجه انتخاب گردد.
+    /// </summary>
+    Auto = 2
+}
